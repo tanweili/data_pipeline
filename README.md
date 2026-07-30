1da498e08178780c59be8a7ddac7472698ca40e6
Tan Wei Li <weili.tan@shopee.com>
2026-07-30 17:56:18 +0800

    Data exploration using ipynb

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 To add .gitignore data_pipeline_venv
 python3 -m venv data_pipeline_venv
 source data_pipeline_venv/bin/activate
+pip3 install pandas matplotlib ipykernel notebook
--- ingestion/ingestion.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import pyarrow.parquet as pq
-
-root_file_path = Path(__file__).resolve().parent.parent
-print(root_file_path)
-trips = pq.read_table(root_file_path / 'data' / 'raw' / 'yellow_tripdata_2025-01.parquet')
-trips = trips.to_pandas()
-print(trips.head(100))
-print(trips.info(verbose = True))
